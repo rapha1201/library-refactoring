dd7deceda7b21ce965842902c15c209b0f81840d
rapha1201 <raphaelcarreiro1201@gmail.com>
2026-04-15 21:27:50 -0300

    Commit 10: Replacing conditional with polymorphism - part 3

--- model.py
+++ model.py
@@ -7,13 +7,32 @@ class Price:
         pass
 
 class RegulaPrice(Price):
-    pass
+    def get_charge(self, days_rented: int) -> float:
+        if (days_rented <= 2):
+            return 2
+        return 2 + ( (days_rented - 2) * 1.5)
+
+    def get_frequent_render_points(self, days_rented: int) -> int:
+        return 1
 
 class NewReleasePrice(Price):
-    pass
+    def get_charge(self, days_rented: int) -> float:
+        return days_rented * 3
+
+    def get_frequent_render_points(self, days_rented: int) -> int:
+        points = 1
+        if (days_rented > 1):
+            return points+1
+        return points
 
 class ChildrenPrice(Price):
-    pass
+    def get_charge(self, days_rented: int) -> float:
+        if (days_rented <= 3):
+            return 1.5
+        return 1.5 + ( (days_rented - 3) * 1.5)
+
+    def get_frequent_render_points(self, days_rented: int) -> int:
+        return 1
 
 class Book:
 
@@ -25,7 +44,7 @@ class Book:
         self.title = title
         self.price_code = self.create_price(price_code)
 
-    def create_price(self, price_code: int):
+    def create_price(self, price_code: int) -> Price:
         if price_code == Book.NEW_RELEASE:
             return NewReleasePrice()
         elif price_code == Book.CHILDREN:
@@ -33,27 +52,10 @@ class Book:
         return RegulaPrice()
     
     def get_charge(self, days_rented: int) -> float:
-        amount = 0
-    
-        # determine amounts for each line
-        if self.price_code == Book.REGULAR:
-            amount += 2
-            if days_rented > 2:
-                amount += (days_rented - 2) * 1.5
-        elif self.price_code == Book.NEW_RELEASE:
-            amount += days_rented * 3
-        elif self.price_code == Book.CHILDREN:
-            amount += 1.5
-            if days_rented > 3:
-                amount += (days_rented - 3) * 1.5
-
-        return amount
+        return self.price_code.get_charge(days_rented)
 
     def get_frequent_render_points(self, days_rented: int) -> int:
-        points = 1
-        if self.price_code == Book.NEW_RELEASE and days_rented > 1:
-            points +=1
-        return points
+        return self.price_code.get_frequent_render_points(days_rented)
 
 class Rental:
     def __init__(self, book: Book, days_rented: int):
